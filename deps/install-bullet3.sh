13bef2e7d125b9e7bcfb441054185a1301d2ec27
aziz12344 <53209144+aziz12344@users.noreply.github.com>
2019-07-24 00:22:09 -0700

    Update install-bullet3.sh

--- deps/install-bullet3.sh
+++ deps/install-bullet3.sh
@@ -1,16 +1,1 @@
-git clone https://github.com/bulletphysics/bullet3.git $HOME/bullet3
-
-pushd $HOME/bullet3
-mkdir bullet-build
-cd bullet-build
-cmake .. -G "Unix Makefiles" -DINSTALL_LIBS=ON -DBUILD_SHARED_LIBS=ON
-make -j $(nproc --all)
-sudo make install
-cd ..
-
-BULLET3_DIRECTORY=$(cd `dirname "$0"` && pwd)
-mycmd="$BULLET3_DIRECTORY"
-echo "export BULLET3=$mycmd" >> ~/.bashrc
-source ~/.bashrc
-
-popd
+echo "Hi"
